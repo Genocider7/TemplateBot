413650304934ba63f5dc8ddeb7a3c52d2f0f639e
Filip <blazerwarrior@gmail.com>
2024-11-15 19:47:38 +0100

    Added constants and settings

---
+++ constants.py
@@ -0,0 +1,10 @@
+"""
+File used to store all constants
+These are meant to not be changed, thus are not in the settings file
+"""
+
+from discord import Intents
+
+required_settings = ['app_token']
+settings_filename = 'settings.json'
+discord_intents = Intents.default()
--- templateBot.py
+++ templateBot.py
@@ -1,4 +1,6 @@
-""""""
+"""
+This is a main program file for the bot
+"""
 
 import discord
 
@@ -7,10 +9,14 @@ import json
 
 from Models.ReturnInfo import ReturnInfo
 
-settings = {}
-required_settings = ['app_token']
+from constants import *
 
-def load_settings(setting_path: str = 'settings.json', required_keys: list = []) -> ReturnInfo:
+settings = {}
+client = discord.Client(intents=discord_intents)
+command_tree = discord.app_commands.CommandTree(client)
+
+# Function to load all the settings and assign them to a global variable
+def load_settings(setting_path: str = settings_filename, required_keys: list = []) -> ReturnInfo:
     global settings
     result = ReturnInfo(returnCode=0, Messages={
         1: f'Error: File \"{setting_path}\" not found',
@@ -41,13 +47,47 @@ def load_settings(setting_path: str = 'settings.json', required_keys: list = [])
         result.returnCode = 1
     return result
 
-def main():
+# Easier way to get a setting without worrying if it's been set if it's negligable
+def get_setting(setting_name: str, type_of_setting: type = str) -> str | int | bool:
+    if setting_name in settings.keys():
+        return settings[setting_name]
+    if type_of_setting == str:
+        return ''
+    if type_of_setting == int:
+        return 0
+    if type_of_setting == bool:
+        return False
+    return None
+
+# Sets up commands that require context earlier
+def set_up_functions() -> None:
+    if get_setting('home_guild_id') != '':
+        global turn_off_bot_command
+        @command_tree.command(
+            name='off',
+            description=get_setting('off_command_description'),
+            guild=discord.Object(id=settings['home_guild_id'])
+        )
+        async def turn_off_bot_command(context: discord.Interaction):
+            await context.response.send_message('Turning off bot...')
+            await client.close()
+    else:
+        print('Unable to set up off command')
+
+# Called when bot connects to discord
+@client.event
+async def on_ready() -> None:
+    print(f'Logged in as {client.user.name}')
+    print(client.user.id)
+    set_up_functions()
+
+# Main function of the program
+def main() -> None:
     result = load_settings(required_keys=required_settings)
     if not result:
         print(result)
         return
-    print(settings)
-
+    client.run(settings['app_token'])
 
 if __name__ == '__main__':
     main()
